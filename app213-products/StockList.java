615593f21eb89994e5a2dec7939c70f07f5d5d33
ScrysDev <91500026+ScrysDev@users.noreply.github.com>
2021-11-16 00:10:59 +0000

    final submission
    Unfortunately was not able to complete the app. I could not get the remove item from array list to work and wasn't sure how to search for items based on a name. I didn't manage to get the items shown based on stock level, but I don't think it was displayed as it was requested.

--- app213-products/StockList.java
+++ app213-products/StockList.java
@@ -46,6 +46,14 @@ public class StockList
      */
     public void buyProduct(int productID, int amount)
     {
+        //increase the quantity of a product based on ID 
+        //by the value in amount
+        
+        Product product = findProduct(productID);
+        if(product != null)
+            product.increaseQuantity(amount);
+        else
+            System.out.println("Item could not be found");
     }
     
     /**
@@ -54,38 +62,74 @@ public class StockList
      */
     public Product findProduct(int productID)
     {
+        for (Product product : stock)
+        {
+            if(product.getID() == productID)
+            return product;
+
+        }  
         return null;
     }
     
+    public void sellProduct(int productID)
+    {
+        sellProduct(productID, 1);
+    }
     
     /**
-     * Sell one of the given product.
+     * Sell any amount of the given product.
      * Show the before and after status of the product.
      * @param id The ID of the product being sold.
      */
-    public void sellProduct(int productID)
+    public void sellProduct(int productID, int amount)
+    
+    //created a variable called amount to be used in findProduct
     {
         Product product = findProduct(productID);
         
         if(product != null) 
         {
-            if(product.getQuantity() > 0)
+            // Checks to make sure that the value in amount 
+            if(product.getQuantity() > 0 && product.getQuantity() > amount)
             {
-                product.decreaseQuantity(1);
+                // used the newly created amount variable to store
+                // a value other than 1 that was previously here
+                // to alllow it to be decreaed by any value until 0 
+                product.decreaseQuantity(amount);
                 
-                // printout message
+                System.out.println("An order of " + amount + " copies of " + 
+                product.getName() + " has been made");
+
             }
+            else if (product.getQuantity() == 0)
+            {
+                System.out.println("The item " + product.getName() + 
+                " is out of stock");
+            }
+            
             else
             {
-                // printout message
+                System.out.println("An order of " + product.getName() + 
+                " can not be completed because you have ordered a quantity " +
+                "over our available stock. We have " + product.getQuantity() + " in stock");
             }
         }
+        
         else
         {
-            // printout message
+            System.out.println("Item could not be found");
         }
+        
+        if (product.getQuantity() <=3)
+            {
+                System.out.println(" ====================");
+                System.out.println ("These items are low on stock ");
+                System.out.println (product.getName() + " has " +
+                product.getQuantity() + " in stock");
+                System.out.println(" ====================");
+ 		System.out.println();
+            }
     }    
-
     
     /**
      * Locate a product with the given ID, and return how
@@ -99,6 +143,18 @@ public class StockList
         return 0;
     }
 
+    // Removed as it did not work
+    
+        //public void removeItem(int id)
+    //{
+        //Product product = findProduct(id);
+       // if (product.getQuantity() == 0);
+        //{
+            //stock.remove(product);
+           // System.out.println (product.getName() + " had been removed from our store ");
+           // System.out.println (" Due to it being out of stock. ");
+           // System.out.println (" We will re-add the item once it is back on stock.");
+        //}
     /**
      * Print details of the given product. If found,
      * its name and stock quantity will be shown.
@@ -119,7 +175,7 @@ public class StockList
      * in the order they are in the stock list
      */
     public void print()
-    {
+        {
         printHeading();
         
         for(Product product : stock)
@@ -131,10 +187,12 @@ public class StockList
     }
     
     public void printHeading()
+
     {
         System.out.println();
-        System.out.println(" Peacock's Stock List");
+        System.out.println(" Ben's Stock List");
         System.out.println(" ====================");
         System.out.println();
     }
+    
 }
